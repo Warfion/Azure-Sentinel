1e4c43a2823644132df3de01ba8c99eeeebc6ba3
Warfion <58534974+Warfion@users.noreply.github.com>
2024-08-01 00:45:30 +0200

    Update Get-Tactics.ps1

--- Scripts/Mitre Att&ck/Get-Tactics/Get-Tactics.ps1
+++ Scripts/Mitre Att&ck/Get-Tactics/Get-Tactics.ps1
@@ -1,1 +1,23 @@
+$listsites = Invoke-WebRequest "https://github.com/mitre/cti/tree/master/enterprise-attack/x-mitre-tactic"
 
+$subsites=([regex]::Matches($listsites, '(?<=("path":")).*?(?=")').Value) -match "enterprise-attack/x-mitre-tactic/x-mitre-tactic--"
+
+$tactics = [System.Collections.Generic.List[object]]::new()
+
+foreach($subsite in $subsites){
+
+    $uri = "https://raw.githubusercontent.com/mitre/cti/master/"+$subsite
+    $webData=Invoke-WebRequest -Uri $uri
+    $tactic = ConvertFrom-Json $webData
+    
+    $tactic = [PSCustomObject]@{
+        Name = $tactic.objects.name
+        ID = $tactic.objects.external_references.external_id
+        Description = $tactic.objects.description
+    } 
+    $tactics.Add($tactic)
+}
+
+$tactics
+#$tactics | Export-Csv -NoTypeInformation "tactics.csv"
+#$tactics | ConvertTo-Json -depth 100 | Out-File "tactics.json"
